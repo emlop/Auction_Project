1e5818f2d83c39b434121d6942638c5325cbc9cb
emel lopez <emlopez88@gmail.com>
2017-05-02 19:09:43 -0400

    updated app.js

---
+++ auction_app/app.js
@@ -0,0 +1,17 @@
+/* setting up express */
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+/* this will get the environment variable in to .env file */
+require('dotenv').config();
+
+const app = express();
+
+/* importing routes */
+const 
