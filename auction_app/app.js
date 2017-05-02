c6e14bca63e25c386afa36e16e524dc8ad8e6fe0
emel lopez <emlopez88@gmail.com>
2017-05-02 19:46:26 -0400

    entered code to app.js

--- auction_app/app.js
+++ auction_app/app.js
@@ -13,5 +13,47 @@ require('dotenv').config();
 
 const app = express();
 
-/* importing routes */
-const 
+/* importing route */
+const auctionRoutes = require('.routes/auctions');
+
+/* setting up port & listen */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log(`listening on port ${PORT}`);
+});
+
+/* setting up views */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/* setting static file */
+app.use('/static', express.static(path.join(__dirname, 'public')));
+/* setting up logger */
+app.use(logger('dev'));
+app.use(cookieParser());
+/* setting up body-parser */
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: process.env.SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* setting up routes */
+app.get('/', function(req, res){
+  res.render('index', {
+    message: 'Hola',
+    documentTitle: 'Auction App',
+    subTitle: 'Selling you old stuff here',
+  });
+});
+app.use('/auctions', auctionRoutes);
+
+/* handling 404 */
+app.get('*', function(req, res) {
+  res.status(404).send({message: 'Oops! Not Found.' });
+});
