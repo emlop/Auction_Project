0298caae916e7fb7f200ab553b54e60b1e09fefb
emel lopez <emlopez88@gmail.com>
2017-05-07 22:43:14 -0400

    cleaned up files

--- auction_app/app.js
+++ auction_app/app.js
@@ -4,12 +4,6 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-// const cookieParser = require('cookie-parser');
-// const session = require('express-session');
-// const passport = require('passport');
-
-/* this will get the environment variable in to .env file */
-// require('dotenv').config();
 
 const app = express();
 
